473da89219f2b20e50e276bdbb47ef6965b9cb6d
Vinayak <vinayakskye@gmail.com>
2021-08-23 20:30:34 +0530

    Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.json.JSONException;
+
+public class Main{
+
+	public static void main(String[] args) throws FileNotFoundException, JSONException {
+		FirstPageGUI gui = new FirstPageGUI();
+	}
+
+}
